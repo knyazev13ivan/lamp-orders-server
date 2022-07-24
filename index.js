5f91bbc07bffb637aa0274fded9ad2c2b3f9e002
Ivan Knyazev <knyazev13ivan@gmail.com>
2022-07-24 14:57:13 +0300

    добавил контроллеры для регистриции и авторизации

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 import express from "express";
-import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 
+import { validationResult } from "express-validator";
+
+import {
+  registerValidation,
+  loginValidation,
+  ordersInLineCreateValidation,
+} from "./validations.js";
+
+import checkAuth from "./utils/checkAuth.js";
+
+import * as UserController from "./controllers/UserController";
+
 mongoose
-  .connect('mongodb+srv://admin:lampadmin@cluster0.a1mukxo.mongodb.net/?retryWrites=true&w=majority')
-  .then(() => console.log('Database OK'))
-  .catch((err) => console.log('Database ERROR', err));
+  .connect(
+    "mongodb+srv://admin:lampadmin@cluster0.a1mukxo.mongodb.net/lamps?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Database OK"))
+  .catch((err) => console.log("Database ERROR", err));
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Test Hello! 2ddd');
-});
-
-app.post('/auth/login', (req, res) => {
-
-  const token = jwt.sign({
-    login: req.body.login,
-    fullName: 'Test Name'
-  }, 'supersecret');
-
-  res.json({
-    success: true,
-  });
-});
+app.post("/auth/login", loginValidation, UserController.login);
+app.post("/auth/register", registerValidation, UserController.register);
+app.get("/auth/me", checkAuth, UserController.getMe);
 
 app.listen(3001, (err) => {
   if (err) {
     return console.log(err);
   }
 
-  console.log('Server ok');
+  console.log("Server ok");
 });
---
+++ validations.js
@@ -0,0 +1,23 @@
+import { body } from "express-validator";
+
+export const loginValidation = [
+  body("login", "Неверный формат логина").isLength({ min: 5 }).isString(),
+  body("password", "Пароль должен быть минимум 5 символов").isLength({
+    min: 5,
+  }),
+];
+
+export const registerValidation = [
+  body("login", "Неверный формат логина").isLength({ min: 5 }).isString(),
+  body("password", "Пароль должен быть минимум 5 символов").isLength({
+    min: 5,
+  }),
+  body("fullName", "Укажите имя").isLength({ min: 3 }),
+];
+
+export const ordersInLineCreateValidation = [
+  body("name", "Введите тип фонаря").isLength({ min: 3 }).isString(),
+  body("number", "Введите количество фонарей в заказе").isNumber(),
+  body("priority", "Выберите приоритет").isString(),
+  body("text", "Введите описание").optional().isString(),
+];
